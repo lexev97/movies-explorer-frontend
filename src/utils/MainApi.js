57140a39f870a9381aa3396bb12c9eb3d8f318a7
Lexev <evtenkoaleksei@gmail.com>
2023-10-14 00:48:03 +0300

    hot fixes

--- src/utils/MainApi.js
+++ src/utils/MainApi.js
@@ -4,7 +4,7 @@ class MainApi {
     // this._baseUrl = 'http://localhost:5000';
     this._headers = {
       'Content-Type': 'application/json',
-      'Referrer-Policy': 'origin-when-cross-origin',
+      'Origin': 'https://ypdiploma.nomoreparties.co',
     };
   }
 
